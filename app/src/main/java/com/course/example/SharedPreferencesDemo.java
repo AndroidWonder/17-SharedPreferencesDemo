3435e6d02689db198ac829b312bccb4090bf3c6d
Android Wonder <jim.pepe@verizon.net>
2022-03-12 10:20:49 -0500

    update rev

--- app/src/main/java/com/course/example/SharedPreferencesDemo.java
+++ app/src/main/java/com/course/example/SharedPreferencesDemo.java
@@ -15,7 +15,6 @@ import android.widget.Toast;
 import android.content.SharedPreferences;
 import android.content.Intent;
 import android.view.View;
-import android.view.View.OnClickListener;
 
 public class SharedPreferencesDemo extends Activity implements View.OnClickListener {
  
